827f96ff9c6cdf8dc60326c1dfdc871945d22acd
meetchotaliya895-commits <meetchotaliya895@gmail.com>
2026-01-03 11:33:11 +0530

    Add files via upload

---
+++ one.py
@@ -0,0 +1,28 @@
+print("hellow word")
+
+a = 12
+print(a)
+
+b = 12.3 
+print(b)
+
+c = True
+print(c)
+
+d = ["name","roll","email"]
+print(d)
+
+e = {"a","b","c","d"}
+print(e)
+
+f = 1j
+print(f)
+
+g = "meet"
+print(g)
+
+h = None
+print(h)
+
+
+
