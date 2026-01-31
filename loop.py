0a703b14e450ea28aa2e41f3ba7db923d4482b18
meetchotaliya895-commits <meetchotaliya895@gmail.com>
2026-01-31 11:28:00 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,14 @@
+age = int(input("enter your age:"))
+if age>=18:
+    print("yes you are eligible for vote")
+else:
+    print("sorry you are not eligible for vote")
+    
+marks = int(input("enter your marks:"))
+if marks>=90:
+    print("you will det grade a")
+elif marks>=60:
+    print("you will det grade b")
+else:
+    print("better luck next time")
+    
