830f2657c2db959fd862802335758eedd7e142ff
meetchotaliya895-commits <meetchotaliya895@gmail.com>
2026-01-31 10:44:58 +0530

    Add files via upload

---
+++ I_O.py
@@ -0,0 +1,11 @@
+# # Taking input from the user
+# name = input("Enter your name: ")
+# age =  input("Enter your age: ")  
+
+# # Output
+# print("Your name is ", name)
+# print("You Age is ", age)
+
+
+import sys
+print(sys.argv)
