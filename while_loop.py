4be8b7a0f6106804880de2f2e8254e4261a5ee54
meetchotaliya895-commits <meetchotaliya895@gmail.com>
2026-03-07 11:25:43 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,27 @@
+n = 0
+while (True):
+        print(n)
+        n += 1 
+        if(n==5):
+            break
+    
+n=0
+while(n !=5):
+    print(n)
+    n+=1
+
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+    
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+    
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
