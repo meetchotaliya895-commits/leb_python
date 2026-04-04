b712646e1decedefbfe790c195655b67235e6344
meetchotaliya895-commits <meetchotaliya895@gmail.com>
2026-04-04 10:00:21 +0530

    Add files via upload

---
+++ keyword_arg.py
@@ -0,0 +1,19 @@
+def student(name, age ,city):
+    print("Name:", name)
+    print("Age:", age)
+    print("city:",city)
+
+student(name="ravi", age=18 , city="rajkot")
+
+def info(city, country):
+    print("City:", city)
+    print("Country:", country)
+
+info(country="India", city="Rajkot")
+
+def display(a, b ,c):
+    print("a=",a)
+    print("b=",b)
+    print("c=",c)
+
+display(1,c=3 , b=2)
